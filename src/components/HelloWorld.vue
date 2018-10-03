dcdd2edc1e75ee3cce466414af7a305bf3d290b4
Rajith Wijepura <rajith@benworldwide.com>
2018-10-03 09:09:49 +0530

    first commit from project

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>{{ msg }} {{test}} </h1>
     <p>
       For guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -34,7 +34,8 @@
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
+    msg: String,
+    test: Number
   }
 }
 </script>
